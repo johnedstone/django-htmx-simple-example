429aceba1b90e93d3b8ef55f62936830c0a36752
johnedstone <johnedstone@gmail.com>
2024-03-04 19:03:40 -0500

    working on transition of htmx

--- Readme.md
+++ Readme.md
@@ -1,5 +1,18 @@
+### Run this app
+```
+django-admin runserver --pythonpath=. --settings=tinyapp
+```
+
 ### References
+
 * [https://dev.to/johntellsall/fast-web-minimal-django-p17](https://dev.to/johntellsall/fast-web-minimal-django-p17)
 ```
     django-admin runserver --pythonpath=. --settings=tinyapp
 ```
+
+* [https://github.com/chriswedgwood/django-htmx-examples/tree/main/django_htmx_examples](https://github.com/chriswedgwood/django-htmx-examples/tree/main/django_htmx_examples)
+* [https://dev.to/arthurobo/django-htmx-load-more-objects-3jg3](https://dev.to/arthurobo/django-htmx-load-more-objects-3jg3)
+_Used Bootstrap CSS for styling this project_
+
+* [https://htmx.org/examples/lazy-load/](https://htmx.org/examples/lazy-load/)
+* [https://livebook.manning.com/book/django-in-action/chapter-12/v-6/14](https://livebook.manning.com/book/django-in-action/chapter-12/v-6/14)
--- tinyapp.py
+++ tinyapp.py
@@ -1,9 +1,33 @@
+import os
+import secrets
+import logging
+from datetime import datetime
+
 from django.urls import re_path
 from django.shortcuts import redirect, render as django_render
 
 DEBUG = True
-SECRET_KEY = '1234'
+SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_urlsafe(32))
+if not DEBUG:
+    ALLOWED_HOSTS = ['*',]
+
+logging.debug(f'SECRET_KEY: {SECRET_KEY}')
+
 ROOT_URLCONF = __name__
+
+APPS = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+    'django-htmx',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -12,6 +36,9 @@ TEMPLATES = [
         ],
     },
 ]
+
+STATIC_URL = "static/"
+STATIC_ROOT = 'static'
 
 # wrapper renders django template
 def render(template_name):
@@ -23,19 +50,29 @@ def render(template_name):
     return decorator
 
 
-@render(template_name='about.html')
+@render(template_name='latest_data.html')
 def about(request):
-    title = 'Tinyapp'
-    author = 'John Mitchell'
+    current_datetime = datetime.now()
     return locals()
 
 def home(request):
-    return redirect('aboutpage')
+    return redirect('latest_data')
+
+@render(template_name='partial_attendees.html')
+def attendees(request):
+    attendees='Two Thousand'
+    return locals()
 
 
 urlpatterns = [
     re_path(r'^$', home, name='homepage'),
-    re_path(r'^about/$', about, name='aboutpage'),
+    re_path(r'^latest-data/$', about, name='latest_data'),
+    re_path(r'^latest-data/attendees/$', attendees, name='attendees'),
 ]
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # vim: ai et ts=4 sw=4 sts=4 nu
